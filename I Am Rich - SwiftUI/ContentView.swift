67762ced7f465ebd79ef58ea5f5c64d1aad7eadc
nishanttaneja <nishant.taneja2701@gmail.com>
2020-09-01 02:02:31 +0530

    Initial Commit

---
+++ I Am Rich - SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich - SwiftUI
+//
+//  Created by Nishant Taneja on 01/09/20.
+//  Copyright © 2020 Nishant Taneja. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
